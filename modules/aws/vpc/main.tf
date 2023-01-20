00f7ce814505f9e7bea21e28089ab49ac50dc079
Piotr Boruszczak <piotr.boruszczak@gmail.com>
2023-01-20 18:33:23 +0100

    added rt, igw and rt assoc

--- modules/aws/vpc/main.tf
+++ modules/aws/vpc/main.tf
@@ -54,7 +54,7 @@ resource "aws_subnet" "public" {
 resource "aws_internet_gateway" "this" {
   for_each = {
     for k, v in var.vpcs : k => v
-    if length(v.private_subnets) > 0
+    if length(v.public_subnets) > 0
   }
 
   vpc_id = aws_vpc.this[each.key].id
@@ -68,14 +68,56 @@ resource "aws_internet_gateway" "this" {
   )
 }
 
-# resource "aws_route" "public_internet_gateway" {
-#   count = local.create_vpc && var.create_igw && length(var.public_subnets) > 0 ? 1 : 0
+resource "aws_route_table" "public" {
+  for_each = local.public_subnets
 
-#   route_table_id         = aws_route_table.public[0].id
-#   destination_cidr_block = "0.0.0.0/0"
-#   gateway_id             = aws_internet_gateway.this[0].id
+  vpc_id = aws_vpc.this[each.key].id
 
-#   timeouts {
-#     create = "5m"
-#   }
-# }
+  tags = merge(
+    {
+      "Name" = each.key
+    },
+    each.value.tags,
+    var.common_tags
+  )
+}
+
+resource "aws_route_table" "private" {
+  count = local.private_subnets
+
+  vpc_id = aws_vpc.this[each.key].id
+
+  tags = merge(
+    {
+      "Name" = each.key
+    },
+    each.value.tags,
+    var.common_tags
+  )
+}
+
+resource "aws_route" "public_igw" {
+  for_each = local.public_subnets
+
+  route_table_id         = aws_route_table.public[each.key].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this[each.value.vpc_name].id
+
+  timeouts {
+    create = "5m"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  for_each = local.public_subnets
+
+  subnet_id      = aws_subnet.public[each.key].id
+  route_table_id = aws_route_table.public[each.key].id
+}
+
+resource "aws_route_table_association" "private" {
+  for_each = local.private_subnets
+
+  subnet_id      = aws_subnet.private[each.key].id
+  route_table_id = aws_route_table.private[each.key].id
+}
