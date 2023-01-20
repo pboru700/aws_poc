5f1669c5ea604db315b412396ad80e5fc776f84c
Piotr Boruszczak <piotr.boruszczak@gmail.com>
2023-01-20 20:07:14 +0100

    added vpc deployment

---
+++ live/prod/us-east-1/vpc/main.tf
@@ -0,0 +1,28 @@
+module "vpc" {
+  source = "git@github.com:pboru700/aws_poc.git//modules/aws/vpc?ref=vpc1.0.0"
+
+  vpcs = {
+    prod = {
+      cidr = "10.0.0.0/16"
+      secondary_cidr_blocks = []
+      public_subnets = {
+        "us-east-1a" = "10.0.0.0/24"
+        "us-east-1b" = "10.0.1.0/24"
+        "us-east-1c" = "10.0.2.0/24"
+      }
+      private_subnets = {
+        "us-east-1a" = "10.0.10.0/24"
+        "us-east-1b" = "10.0.11.0/24"
+        "us-east-1c" = "10.0.12.0/24"
+      }
+
+      tags = {
+        "Purpose" = "POC"
+      }
+    }
+  }
+
+  common_tags = {
+    Environment = "prod"
+  }
+}
---
+++ live/test/us-east-1/vpc/main.tf
@@ -0,0 +1,28 @@
+module "vpc" {
+  source = "git@github.com:pboru700/aws_poc.git//modules/aws/vpc?ref=vpc1.0.0"
+
+  vpcs = {
+    test = {
+      cidr = "10.10.0.0/16"
+      secondary_cidr_blocks = []
+      public_subnets = {
+        "us-east-1a" = "10.10.0.0/24"
+        "us-east-1b" = "10.10.1.0/24"
+        "us-east-1c" = "10.10.2.0/24"
+      }
+      private_subnets = {
+        "us-east-1a" = "10.10.10.0/24"
+        "us-east-1b" = "10.10.11.0/24"
+        "us-east-1c" = "10.10.12.0/24"
+      }
+
+      tags = {
+        "Purpose" = "POC"
+      }
+    }
+  }
+
+  common_tags = {
+    Environment = "test"
+  }
+}
