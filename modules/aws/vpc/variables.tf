57952769dd174e4849dce320d4be2ad87c2370e8
Piotr Boruszczak <piotr.boruszczak@gmail.com>
2023-01-21 15:30:48 +0100

    removed vpc unused variables

--- modules/aws/vpc/variables.tf
+++ modules/aws/vpc/variables.tf
@@ -9,15 +9,3 @@ variable "common_tags" {
   default     = {}
   description = "Map of tags common among every resource"
 }
-
-variable "private_subnets" {
-  type        = map(string)
-  default     = {}
-  description = "Map of private subnets to create"
-}
-
-variable "public_subnets" {
-  type        = map(string)
-  default     = {}
-  description = "Map of public subnets to create"
-}
